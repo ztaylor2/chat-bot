bf59b36993e673c79b231e35f41efb452d6a3729
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-03-27 05:46:02 -0700

    added init conversation function.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 """Bot flask app."""
 from microsoftbotframework import MsBot
-from tasks import handle_response
+from tasks import handle_response, init_conversation
 
 bot = MsBot()
+bot.add_process(init_conversation)
 bot.add_process(handle_response)
 
 if __name__ == '__main__':
--- tasks.py
+++ tasks.py
@@ -1,5 +1,5 @@
 """Bot responses."""
-from microsoftbotframework import ReplyToActivity
+from microsoftbotframework import ReplyToActivity, SendToConversation
 import os
 import requests
 
@@ -10,6 +10,13 @@ import json
 
 luis_key = os.environ.get('LUIS_KEY')
 text_analytics_key = os.environ.get('TEXT_ANALYTICS_KEY')
+
+
+def init_conversation(message):
+    """Begin conversation."""
+    SendToConversation(fill=message,
+                       conversationId='jg3alifjua8sdljn9abiuao4ihbimroivb',
+                       text='Hi! I\'m a chat bot designed to help you find movies. Feel free to search for movies by actor, title, genre, or keyword.').send()
 
 
 def handle_response(message):
